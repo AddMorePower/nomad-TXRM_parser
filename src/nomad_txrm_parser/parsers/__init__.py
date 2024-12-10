0a68c5e9ccc8dd6665ad2353563040fde2b85440
Guillaume Gaisne <guillaume.gaisne@esrf.fr>
2024-12-10 14:50:20 +0100

    Creation of the TXRM parser

--- src/nomad_txrm_parser/parsers/__init__.py
+++ src/nomad_txrm_parser/parsers/__init__.py
@@ -1,9 +1,7 @@
 from nomad.config.models.plugins import ParserEntryPoint
-from pydantic import Field
 
 
 class NewParserEntryPoint(ParserEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
         from nomad_txrm_parser.parsers.parser import NewParser
@@ -12,7 +10,7 @@ class NewParserEntryPoint(ParserEntryPoint):
 
 
 parser_entry_point = NewParserEntryPoint(
-    name='NewParser',
-    description='New parser entry point configuration.',
-    mainfile_name_re='.*\.newmainfilename',
+    name='TXRMParser',
+    description='Parser for .txrm files from IKTS.',
+    mainfile_name_re='.*\.txrm',
 )
